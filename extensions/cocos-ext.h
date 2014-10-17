0960ebebf952c305fd51b1e4521bfaa39bba273c
SunLightJuly <ygqy8181@qq.com>
2014-10-17 18:36:11 +0800

    cleanup

--- extensions/cocos-ext.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COCOS2D_EXT_H__
-#define __COCOS2D_EXT_H__
-
-#include "ExtensionMacros.h"
-
-#include "GUI/CCEditBox/CCEditBox.h"
-#include "GUI/CCTextField/UITextField.h"
-
-#endif /* __COCOS2D_EXT_H__ */
